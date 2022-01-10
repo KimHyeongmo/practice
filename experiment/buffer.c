71e6d7f1d8c60812cb1ca8a1b7f7de96e1d79587
KimHyeongmo <hngmo.kim@gmail.com>
2022-01-10 00:48:39 -0800

    object file ignore test

---
+++ experiment/buffer.c
@@ -0,0 +1,12 @@
+/*
+This program is testing for stdin buffer.
+When I write input data like "hi hi hi .....", I want to store this data a1 = hi, a2 = hi, a3 = hi.
+Sequential string datas have to ignore space and splitted.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hi");
+}
